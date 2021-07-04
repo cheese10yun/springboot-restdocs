47378ef85c277624139965e7bf8020693532c171
cheese10yun <cheese10yun@gmail.com>
2021-07-04 12:28:39 +0900

    Modify rename

--- src/main/java/com/rest/docs/springbootrestdocs/RestdocsApplication.java
+++ src/main/java/com/rest/docs/springbootrestdocs/RestdocsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootRestdocsApplication {
+public class RestdocsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootRestdocsApplication.class, args);
+        SpringApplication.run(RestdocsApplication.class, args);
     }
 
 }
